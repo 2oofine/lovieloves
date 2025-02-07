e96da4d1184db92afd77f9f06151782fbc0dc3cd
sh3rwin21 <sherwin@3enix.com>
2025-02-08 00:26:48 +0800

    tailwind

--- src/components/Lovie/LovieStart/LovieStart.tsx
+++ src/components/Lovie/LovieStart/LovieStart.tsx
@@ -24,7 +24,7 @@ const LovieStart = () => {
           alt="First interaction with lovie"
           height={720}
           width={1400}
-          className="mx-auto object-cover rounded-2xl h-full md:h-[700] lg:h-[1200] object-left-bottom "
+          className="mx-auto object-cover rounded-2xl h-full md:h-[80vh] lg:h-[110vh] object-left-bottom "
           draggable={false}
         />
       </ContainerScroll>
